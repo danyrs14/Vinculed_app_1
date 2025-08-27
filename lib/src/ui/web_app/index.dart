e83c608eedb483e2db8ca9d3463b4cf1d37b35de
danyrs14 <eminemrs14@gmail.com>
2025-08-27 10:55:14 -0600

    page error 404

--- lib/src/ui/web_app/index.dart
+++ lib/src/ui/web_app/index.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:vinculed_app_1/src/ui/web_app/dashboard.dart';
 import 'package:vinculed_app_1/src/ui/web_app/login.dart';
+import 'package:vinculed_app_1/src/ui/web_app/not_found_page.dart';
 import 'package:vinculed_app_1/src/ui/web_app/signin.dart';
 
 class AdminApp extends StatelessWidget {
@@ -25,6 +26,11 @@ class AdminApp extends StatelessWidget {
         ),
       ],
       initialLocation: '/dashboard',
+      errorPageBuilder: (context, state) {
+        return MaterialPage(
+          child: NotFoundPage(),
+        );
+      },
     );
 
     return MaterialApp.router(
---
+++ lib/src/ui/web_app/not_found_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:vinculed_app_1/src/ui/widgets/buttons/simple_buttons.dart';
+
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Tu animación Lottie
+            SizedBox(
+              height: 500,
+              width: 500,
+              child: Lottie.asset(
+                'assets/images/error404.json', // Asegúrate de ponerlo en pubspec.yaml
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              "Página no encontrada",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 12),
+            SimpleButton(
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/dashboard');
+              },
+              title: 'Volver al Inicio',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
