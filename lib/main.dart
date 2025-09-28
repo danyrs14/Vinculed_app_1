a68a41295b958d3bf5053c9ef4b8bf3590ba653d
AngelOrta <146902560+AngelOrta@users.noreply.github.com>
2025-09-27 23:32:08 -0600

    Configuracion de firebase en el proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:vinculed_app_1/src/ui/pages/splashscreen.dart';
 import 'package:vinculed_app_1/src/ui/web_app/index.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // importante para que funcione con firebase
+  );
   runApp(MyApp());
 }
 
