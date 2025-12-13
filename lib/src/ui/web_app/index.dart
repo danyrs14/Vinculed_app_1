ae46f88656a429494a2d972f284a61aefccd2079
AngelOrta <146902560+AngelOrta@users.noreply.github.com>
2025-12-13 04:31:33 -0600

    gestion de reclutadores web local
    y ajustes visuales a gestion de alumnos

--- lib/src/ui/web_app/index.dart
+++ lib/src/ui/web_app/index.dart
@@ -7,6 +7,7 @@ import 'package:vinculed_app_1/src/core/providers/user_provider.dart';
 import 'package:vinculed_app_1/src/ui/web_app/admin_gestion_alumnos.dart';
 //import 'package:vinculed_app_1/src/ui/pages/reclutador/perfil_visible.dart';
 import 'package:vinculed_app_1/src/ui/web_app/admin_gestion_empresas.dart';
+import 'package:vinculed_app_1/src/ui/web_app/admin_gestion_reclutadores.dart';
 
 // Candidato
 import 'package:vinculed_app_1/src/ui/web_app/candidato/add_experiencia.dart';
@@ -190,6 +191,10 @@ class _AdminAppState extends State<AdminApp> {
             GoRoute(
               path: 'alumnos',
               builder: (context, state) => const AdminGestionAlumnosPage(),
+            ),
+            GoRoute(
+              path: 'reclutadores',
+              builder: (context, state) => const AdminGestionReclutadoresPage(),
             ),
           ],
         ),
